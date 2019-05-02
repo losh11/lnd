67f61f51c91749364702da322461637aed488404
Otto Suess <ottosuess@protonmail.com>
2019-05-02 13:05:54 +0200

    generate swift grpc file

--- lnrpc/gen_protos.sh
+++ lnrpc/gen_protos.sh
@@ -7,6 +7,7 @@ protoc -I/usr/local/include -I. \
        -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
        --go_out=plugins=grpc,paths=source_relative:. \
        --swift_out=. \
+       --swiftgrpc_out=. \
        --js_out=import_style=commonjs,binary:. \
        rpc.proto
 
